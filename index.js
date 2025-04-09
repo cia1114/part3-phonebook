0294fde03ae3d1734b773e3488fd5fa5ade5277d
Lucia Sotomayor <cia811114@gmail/com>
2025-04-09 11:24:48 -0700

    phonebook backend, step 10

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())  
+
+//Adding morgan middleware to write log line
+//Using tiny predefined format string
+//app.use(morgan('tiny'))
+
+//Creating a new token 
+morgan.token('body', function (req, res) { return req.method==='POST' ? JSON.stringify(req.body) : ''})
+
+//Using predefined format string
+//const morganOwn = morgan(':method :url :status :res[content-length] - :response-time ms :body')
+
+//Using a custom format function
+const morganOwn = morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.body(req, res)  
+    ].join(' ') 
+})
+app.use(morganOwn)
+
+let persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+    },
+    { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
+  ]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonrbook has info for ${persons.length} people</p><p>${new Date(Date.now())}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find( item => item.id === id)
+
+    if (person)
+        response.json(person)
+    else 
+        response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter( item => item.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({ 
+          error: 'name missing' 
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const found = persons.find( item => item.name === body.name)
+    if (found) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random()*1000)
+    }
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
